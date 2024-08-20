d8bf1a7c5f5dace3423b1f805a180610f758d5a0
roman_chistyh <roman.firsthan909@gmail.com>
2024-08-20 09:11:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './styles/App.css'
+
+function App() {
+  return (
+    
+    <>
+      <header className="header">
+        <nav>
+          <ul>
+            <li><a href="/Auth">Авторизация</a></li>
+            <li><a href="/Radar">Технологии</a></li>
+            <li><a href="/Technology">Радар</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div>
+        
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
